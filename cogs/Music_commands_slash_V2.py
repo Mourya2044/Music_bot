7d916d832e83692dd43455c929eb36f46d667ca1
Mourya <108566691+Mourya2044@users.noreply.github.com>
2025-05-18 00:55:51 +0530

    Update .gitignore and add cookie file for yt-dlp configuration

--- cogs/Music_commands_slash_V2.py
+++ cogs/Music_commands_slash_V2.py
@@ -94,7 +94,8 @@ class Music_Controller(commands.Cog):
                 'default_search': 'auto',
                 'source_address': '0.0.0.0',  # Bind to IPv4 to avoid IPv6 issues
                 'forceurl': True,
-                'skip_download': True
+                'skip_download': True,
+                'cookiefile': 'cookies.txt',  # Use your cookie file if needed
             }
             
             # downloader = yt_dlp.YoutubeDL(ydl_opts)
